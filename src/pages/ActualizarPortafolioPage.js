2a4bc7c39a67ed55b8ab77e3b48a7fdb9ca9f9e4
Cristian David Castro Montes <castrom1998@gmail.com>
2019-11-19 22:57:11 -0500

    Adding ActualizarPortafolioPage

---
+++ src/pages/ActualizarPortafolioPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ActualizarPortafolioContainer from '../containers/ActualizarPortafolioContainer';
+
+const ActualizarPortafolioPage = () => (
+  <div className="container full-h">
+  <>
+    <h2>Actualizar Informacion </h2>
+    <hr />
+
+    <ActualizarPortafolioContainer />
+  </>
+  </div>
+)
+
+export default ActualizarPortafolioPage
