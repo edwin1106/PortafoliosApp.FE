c91ea7d8dd7a4a35b50735c7428e66d6f3e83635
Cristian David Castro Montes <castrom1998@gmail.com>
2019-11-20 00:07:48 -0500

    Custom actividad reducer

--- src/reducers/actividad.js
+++ src/reducers/actividad.js
@@ -11,6 +11,10 @@ const actividad = (state = defaultState, action) =>{
       return { ...state, actividadesData: action.payload};
     case ACTIONS.CREATE_ACTIVIDADES:
       return {...state, actividadesData: [...state.actividadesData, action.payload]}
+    case ACTIONS.UPDATE_ACTIVIDADES:
+      return {...state}
+    case ACTIONS.FETCH_ACTIVIDADES_BY_ID:
+      return {...state, actualizarActividadData: action.payload}
     default: 
       return state; 
   }
