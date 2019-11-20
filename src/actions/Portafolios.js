840bd2808e180a4401b88a876dc3d6b669a642b6
Cristian David Castro Montes <castrom1998@gmail.com>
2019-11-19 22:52:13 -0500

    Adding fetchingPortafolioById and UpdatingPortafolio actions

--- src/actions/Portafolios.js
+++ src/actions/Portafolios.js
@@ -1,6 +1,7 @@
 import ACTIONS from "./types";
-import { getPortafolios, postPortafolios } from "../api/PortafolioApi";
+import { getPortafolios, postPortafolios, putPortafolios, getPortafoliosById } from "../api/PortafolioApi";
 import history from '../history'
+import {processDateAttributes} from '../common/utilities/dataTransformations'
 
 export const fetchPortafolios = (portafolios) => ({
     type: ACTIONS.FETCH_PORTAFOLIOS,
@@ -8,6 +9,15 @@ export const fetchPortafolios = (portafolios) => ({
 })
 export const createPortafolio= (portafolio) => ({
   type: ACTIONS.CREATE_PORTAFOLIOS,
+  payload: portafolio
+})
+
+export const updatePortafolio = () =>({
+  type: ACTIONS.UPDATE_PORTAFOLIOS
+})
+
+export const fetchPortafolioById =(portafolio) =>({
+  type: ACTIONS.FETCH_PORTAFOLIOS_BY_ID,
   payload: portafolio
 })
 
@@ -19,4 +29,15 @@ export const creatingPortafolio= (portafolio) => async(dispatch) =>{
   const response = await postPortafolios(portafolio)
   dispatch(createPortafolio(response))
   history.push(`/Portafolios`)
+}
+
+export const updatingPortafolio = (portafolio) => async()=>{
+  await putPortafolios(portafolio)
+  history.push(`/Portafolios`)
+}
+
+export const fetchingPortafolioById = (id) => async(dispatch)=>{
+  const response = await getPortafoliosById(id);
+  processDateAttributes(response, ['fechaInicio']);
+  dispatch(fetchPortafolioById(response))
 }
