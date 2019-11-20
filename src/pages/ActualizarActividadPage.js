00eafb89231504d9cbb0717c692ea07bb303a66b
Cristian David Castro Montes <castrom1998@gmail.com>
2019-11-20 00:06:18 -0500

    Adding ActualizarActividadPage

---
+++ src/pages/ActualizarActividadPage.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ActualizarActividadContainer from '../containers/ActualizarActividadContainer';
+
+const ActualizarActividadPage = () => (
+  <div className="container full-h">
+  <>
+    <h2>Actualizar Información </h2>
+    <hr />
+
+    <ActualizarActividadContainer />
+  </>
+  </div>
+)
+
+export default ActualizarActividadPage
