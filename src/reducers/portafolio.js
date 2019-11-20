e2ec350ff2b1a362e7c39c9cc04d8dbbf9ba2159
Cristian David Castro Montes <castrom1998@gmail.com>
2019-11-19 22:56:12 -0500

    Updating portafolio reducer

--- src/reducers/portafolio.js
+++ src/reducers/portafolio.js
@@ -11,6 +11,10 @@ const portafolio = (state = defaultState, action) =>{
       return { ...state, portafoliosData: action.payload};
     case ACTIONS.CREATE_PORTAFOLIOS:
       return { ...state, portafoliosData: [...state.portafoliosData, action.payload]}
+    case ACTIONS.UPDATE_PORTAFOLIOS:
+      return {...state}
+    case ACTIONS.FETCH_PORTAFOLIOS_BY_ID:
+      return {...state, actualizarPortafolioData: action.payload}
     default: 
       return state; 
   }
